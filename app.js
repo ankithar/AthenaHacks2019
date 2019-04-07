868574b067e382b7e0dd10d5cc526d4a00dad4f6
ankithar <ankithar.12@gmail.com>
2019-04-07 07:38:03 -0700

    app.js modified

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 var express = require("express");
+var querystring = require('querystring');
+var bodyParser = require('body-parser');
+
 var app = express();
 app.use(express.static("./public"));
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-/*
-app.get('https://api.edamam.com/search?q=onion,carrot&app_id=3529c511&app_key=25af165f14270714d02cd98ebfd43c1c&from=0&to=3', function(request, response) {
-  response.send();
-});
-*/
+
+var app_id = "3529c511";
+var app_key = "25af165f14270714d02cd98ebfd43c1c";
+var from = 0;
+var to = 5;
+
+
+var rp = require('request-promise');
 
 app.get('/service', function(request, response) {
-  //console.dir('working');
-    var Request = require("request");
-
-Request.get("https://api.edamam.com/search?q=onion,carrot&app_id=3529c511&app_key=25af165f14270714d02cd98ebfd43c1c&from=0&to=3", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
+    var ingredients = request.query.ingredients;
+    var options = {
+        uri: "https://api.edamam.com/search?q="+ingredients+"&app_id="+app_id+
+"&app_key="+app_key+"&from="+from+"&to="+to,
+        
+        json: true // Automatically parses the JSON string in the response
+};
+    
+    rp(options)
+    .then(function (repos) {
+        console.log(repos);
+        response.send(repos);
+    })
+    .catch(function (err) {
+        // API call failed...
+    });
 });
-});
-
-/*Working
-var Request = require("request");
-
-Request.get("https://api.edamam.com/search?q=onion,carrot&app_id=3529c511&app_key=25af165f14270714d02cd98ebfd43c1c&from=0&to=3", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
-*/
